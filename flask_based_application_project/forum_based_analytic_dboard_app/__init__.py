097b0fa57f982eab2852c701d6e127caefbd7399
Monish-Mathpal <monishmathpal@gmail.com>
2020-06-20 01:42:10 +0530

    added session expire log out feature, yet to be tested

--- flask_based_application_project/forum_based_analytic_dboard_app/__init__.py
+++ flask_based_application_project/forum_based_analytic_dboard_app/__init__.py
@@ -5,16 +5,21 @@ from flask_login import LoginManager
 from flask_restful import(Api, Resource, fields, marshal, reqparse)
 from flask import Flask, jsonify, url_for, abort
 from flask_migrate import Migrate
-
+from .views.reports import reports
+from flask import Flask, session
+from datetime import timedelta
 app = Flask(__name__)
 api = Api(app)
 
 UPLOAD_FOLDER_PATH = './docs/'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
+
+
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./db/site.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER_PATH
+app.register_blueprint(reports)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
 
@@ -24,6 +29,14 @@ migrate = Migrate(app, db)
 # What is the role of LoginManager class
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+login_manager.refresh_view = 'relogin'
+login_manager.needs_refresh_message_category = "info"
+login_manager.needs_refresh_message = (u"Session timedout, please re-login")
 
-from forum_based_analytic_dboard_app.view import routes
-from forum_based_analytic_dboard_app.models import models
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+
+from .views import routes, reports
+from .models import models
