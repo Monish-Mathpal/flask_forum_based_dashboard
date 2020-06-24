4417381d63b46a0ee8bb7234565fcd085a46e869
Monish-Mathpal <monishmathpal@gmail.com>
2020-06-25 00:44:57 +0530

    fixed ajax call cors issue

--- flask_based_application_project/api/main.py
+++ flask_based_application_project/api/main.py
@@ -5,10 +5,27 @@ from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 # Dependency
